399229614fbeb414af40af28bd5adb76ad666b16
franzholz <franzholz@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-12-27 08:52:36 +0000

    version 0.0.3 from TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/transactor/trunk@41565 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "transactor"
+# Extension Manager/Repository config file for ext "transactor".
 #
-# Auto generated 31-12-2009 09:34
+# Auto generated 27-12-2010 09:49
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'This is a basic API to develop extensions which connect to different payment transaction gateways.',
 	'category' => 'misc',
 	'shy' => 0,
-	'version' => '0.0.2',
+	'version' => '0.0.3',
 	'dependencies' => 'cms,div2007',
 	'conflicts' => '',
 	'priority' => '',
@@ -44,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:46:{s:9:"Changelog";s:4:"3f0c";s:10:"README.txt";s:4:"18e4";s:8:"TODO.txt";s:4:"deaf";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"187e";s:14:"ext_tables.sql";s:4:"40f5";s:13:"locallang.xml";s:4:"0bd6";s:16:"locallang_db.php";s:4:"eea9";s:7:"tca.php";s:4:"484b";s:31:"lib/class.tx_transactor_api.php";s:4:"37af";s:26:"res/icons/BankAxess_03.gif";s:4:"3578";s:25:"res/icons/aktia_large.gif";s:4:"0031";s:24:"res/icons/amex_large.gif";s:4:"e426";s:24:"res/icons/card6_l_03.gif";s:4:"42c7";s:24:"res/icons/card8_l_03.gif";s:4:"4e0c";s:25:"res/icons/dan53-35_01.gif";s:4:"b318";s:17:"res/icons/ddb.gif";s:4:"cdc9";s:23:"res/icons/edk_large.gif";s:4:"781f";s:23:"res/icons/elec_stor.gif";s:4:"c000";s:18:"res/icons/eolv.gif";s:4:"3419";s:23:"res/icons/ewi_large.gif";s:4:"0065";s:22:"res/icons/fk_large.gif";s:4:"8e7e";s:30:"res/icons/getitcard2_53x33.gif";s:4:"b70e";s:30:"res/icons/handelsbanken_01.gif";s:4:"0aa7";s:25:"res/icons/ideal_large.jpg";s:4:"b04d";s:26:"res/icons/jcb-large_01.gif";s:4:"3b57";s:27:"res/icons/jsecure_large.gif";s:4:"1c68";s:21:"res/icons/mc_3d_l.gif";s:4:"7307";s:25:"res/icons/mc_large_01.gif";s:4:"093e";s:22:"res/icons/mstro_01.gif";s:4:"fd6c";s:26:"res/icons/nordea_large.gif";s:4:"951d";s:23:"res/icons/oko_large.gif";s:4:"3b69";s:25:"res/icons/sampo_large.gif";s:4:"6227";s:20:"res/icons/seb_02.gif";s:4:"58c6";s:22:"res/icons/swedbank.gif";s:4:"d6da";s:19:"res/icons/valus.gif";s:4:"6add";s:29:"res/icons/ver_visa2_large.gif";s:4:"5171";s:29:"res/icons/ver_visa_3d_l_1.gif";s:4:"3044";s:23:"res/icons/visa_stor.gif";s:4:"58a5";s:49:"interfaces/interface.tx_transactor_basket_int.php";s:4:"899c";s:50:"interfaces/interface.tx_transactor_gateway_int.php";s:4:"cf61";s:37:"model/class.tx_transactor_gateway.php";s:4:"5e11";s:44:"model/class.tx_transactor_gatewayfactory.php";s:4:"35da";s:42:"model/class.tx_transactor_gatewayproxy.php";s:4:"5e35";s:38:"model/class.tx_transactor_language.php";s:4:"6669";s:24:"template/transactor.tmpl";s:4:"b3a4";}',
+	'_md5_values_when_last_written' => 'a:49:{s:9:"Changelog";s:4:"e0d1";s:10:"README.txt";s:4:"18e4";s:8:"TODO.txt";s:4:"deaf";s:21:"ext_conf_template.txt";s:4:"f918";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"70ba";s:14:"ext_tables.php";s:4:"538b";s:14:"ext_tables.sql";s:4:"05d2";s:13:"locallang.xml";s:4:"de0f";s:16:"locallang_db.php";s:4:"eea9";s:16:"locallang_db.xml";s:4:"8622";s:7:"tca.php";s:4:"dd96";s:49:"interfaces/interface.tx_transactor_basket_int.php";s:4:"899c";s:50:"interfaces/interface.tx_transactor_gateway_int.php";s:4:"6324";s:31:"lib/class.tx_transactor_api.php";s:4:"f4a8";s:37:"model/class.tx_transactor_gateway.php";s:4:"079e";s:44:"model/class.tx_transactor_gatewayfactory.php";s:4:"0969";s:42:"model/class.tx_transactor_gatewayproxy.php";s:4:"e03a";s:38:"model/class.tx_transactor_language.php";s:4:"6669";s:26:"res/icons/BankAxess_03.gif";s:4:"3578";s:25:"res/icons/aktia_large.gif";s:4:"0031";s:24:"res/icons/amex_large.gif";s:4:"e426";s:24:"res/icons/card6_l_03.gif";s:4:"42c7";s:24:"res/icons/card8_l_03.gif";s:4:"4e0c";s:25:"res/icons/dan53-35_01.gif";s:4:"b318";s:17:"res/icons/ddb.gif";s:4:"cdc9";s:23:"res/icons/edk_large.gif";s:4:"781f";s:23:"res/icons/elec_stor.gif";s:4:"c000";s:18:"res/icons/eolv.gif";s:4:"3419";s:23:"res/icons/ewi_large.gif";s:4:"0065";s:22:"res/icons/fk_large.gif";s:4:"8e7e";s:30:"res/icons/getitcard2_53x33.gif";s:4:"b70e";s:30:"res/icons/handelsbanken_01.gif";s:4:"0aa7";s:25:"res/icons/ideal_large.jpg";s:4:"b04d";s:26:"res/icons/jcb-large_01.gif";s:4:"3b57";s:27:"res/icons/jsecure_large.gif";s:4:"1c68";s:21:"res/icons/mc_3d_l.gif";s:4:"7307";s:25:"res/icons/mc_large_01.gif";s:4:"093e";s:22:"res/icons/mstro_01.gif";s:4:"fd6c";s:26:"res/icons/nordea_large.gif";s:4:"951d";s:23:"res/icons/oko_large.gif";s:4:"3b69";s:25:"res/icons/sampo_large.gif";s:4:"6227";s:20:"res/icons/seb_02.gif";s:4:"58c6";s:22:"res/icons/swedbank.gif";s:4:"d6da";s:19:"res/icons/valus.gif";s:4:"6add";s:29:"res/icons/ver_visa2_large.gif";s:4:"5171";s:29:"res/icons/ver_visa_3d_l_1.gif";s:4:"3044";s:23:"res/icons/visa_stor.gif";s:4:"58a5";s:24:"template/transactor.tmpl";s:4:"b3a4";}',
 	'suggests' => array(
 	),
 );
