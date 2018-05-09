773a7427e084ce6218e7966623a2715b43a4ee11
Franz Holzinger <holzinger@ttproducts.de>
2018-05-09 19:29:02 +0200

    * new feature: replace the locallang.xml files by locallang.xlf files

--- ext_emconf.php
+++ ext_emconf.php
@@ -30,7 +30,7 @@ $EM_CONF[$_EXTKEY] = array(
         'depends' => array(
             'php' => '5.5.0-7.99.99',
             'typo3' => '6.2.0-8.99.99',
-            'div2007' => '1.7.16-0.0.0',
+            'div2007' => '1.10.1-0.0.0',
         ),
         'conflicts' => array(
         ),
