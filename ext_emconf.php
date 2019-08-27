757a44ad300901e93426a9ee934851040bf1b0d8
Franz Holzinger <holzinger@ttproducts.de>
2019-08-27 21:57:50 +0200

    * readd ext_autoload.php for TYPO3 6.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = array(
     'title' => 'Payment Transactor API',
     'description' => 'This is a basic API to develop extensions which connect to different payment transaction gateways.',
     'category' => 'misc',
-    'version' => '0.7.2',
+    'version' => '0.7.3',
     'state' => 'stable',
     'uploadfolder' => 0,
     'createDirs' => '',
@@ -19,8 +19,8 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints' => array(
         'depends' => array(
             'php' => '5.6.0-7.99.99',
-            'typo3' => '6.2.0-8.99.99',
-            'div2007' => '1.10.7-0.0.0',
+            'typo3' => '6.2.1-8.99.99',
+            'div2007' => '1.10.16-0.0.0',
         ),
         'conflicts' => array(
         ),
