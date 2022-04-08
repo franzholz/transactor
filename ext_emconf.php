4bb8cc7fe363a5ccfbe6cea247c069cfa42dc5b5
Franz Holzinger <franz.holzinger@festwein.de>
2022-04-08 18:07:34 +0200

    * add compatibility for PHP 8

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Payment Transactor API',
     'description' => 'This is a basic API to develop extensions which connect to different payment transaction gateways.',
     'category' => 'misc',
-    'version' => '0.9.1',
+    'version' => '0.9.2',
     'state' => 'stable',
     'clearcacheonload' => 0,
     'author' => 'Franz Holzinger',
@@ -16,9 +16,9 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => '',
     'constraints' => [
         'depends' => [
-            'php' => '7.3.0-7.4.99',
+            'php' => '7.4.0-8.1.99',
             'typo3' => '7.6.0-11.5.99',
-            'div2007' => '1.11.0-0.0.0',
+            'div2007' => '1.12.0-0.0.0',
         ],
         'conflicts' => [
         ],
