a6a3f66361a81aa916c1f750a73468db31357fb2
Franz Holzinger <franz.holzinger@festwein.de>
2022-04-15 13:09:00 +0200

    -

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,11 +1,15 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
-define('TRANSACTOR_EXT', 'transactor');
+if (!defined ('TRANSACTOR_EXT')) {
+    define('TRANSACTOR_EXT', 'transactor');
+}
 
 if (!defined ('TRANSACTOR_LANGUAGE_PATH')) {
     define('TRANSACTOR_LANGUAGE_PATH', 'EXT:' . TRANSACTOR_EXT . '/Resources/Private/Language/');
 }
 
-define('TRANSACTOR_LANGUAGE_PATH_LL', 'LLL:' . TRANSACTOR_LANGUAGE_PATH);
+if (!defined('TRANSACTOR_LANGUAGE_PATH_LL')) {
+    define('TRANSACTOR_LANGUAGE_PATH_LL', 'LLL:' . TRANSACTOR_LANGUAGE_PATH);
+}
 
